dd42d9289263421e1417f00b35508eb06c25aefa
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-12-23 00:02:04 +0200

    feat: structure features section

---
+++ src/components/landing/FeatureCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+export default function FeaturesCard() {
+  return (
+    <Card className="rounded-none border-2 bg-transparent text-stone-100">
+      <CardHeader>
+        <h3>Red Rooms</h3>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/components/landing/Features.tsx
+++ src/components/landing/Features.tsx
@@ -1,5 +1,23 @@
+import { press_start_2p } from '@/constants/fonts';
 import React from 'react';
 
+import FeaturesCard from './FeatureCard';
+
 export default function Features() {
-  return <div>Features</div>;
+  return (
+    <section className="py-14">
+      <div className="container mx-auto">
+        <header className="mb-12 text-center">
+          <p
+            className={`${press_start_2p.className} mb-3 text-center text-3xl font-medium uppercase text-red-600`}
+          >
+            What awaits you
+          </p>
+        </header>
+        <div className="grid grid-cols-4">
+          <FeaturesCard />
+        </div>
+      </div>
+    </section>
+  );
 }
