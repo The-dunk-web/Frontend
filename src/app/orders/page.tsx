16ca4ed4471997973e31d1fb591641eabd12d826
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-01-07 20:55:57 +0200

    feat: implement orders rout

---
+++ src/app/orders/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function page() {
+  return <div>orders page</div>;
+}
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -4,9 +4,5 @@ import React from 'react';
 export default async function page({ params }: { params: { productId: string } }) {
   const { productId } = await params;
 
-  return (
-    <div className="container mx-auto px-5">
-      <ProductInfo productId={productId} />
-    </div>
-  );
+  return <ProductInfo productId={productId} />;
 }
