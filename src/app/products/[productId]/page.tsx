28299e21934574440a76653009c1e6fb2c08fdf3
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-01-17 09:38:19 +0200

    refactor: remove fetch logic

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -5,23 +5,5 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL!;
 export default async function page({ params }: { params: { productId: string } }) {
   const { productId } = await params;
 
-  try {
-    const res = await fetch(`${API_URL}/api/orders/create/${productId}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        quantity: 2,
-      }),
-      credentials: 'include',
-    });
-    const data = await res.json();
-    console.log(res);
-    console.log(data);
-  } catch (err) {
-    console.log(err);
-  }
-
   return <ProductInfo productId={productId} />;
 }
