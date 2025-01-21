173b65377d3ebf4828a6a4e1850e48432b3676f5
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-01-21 22:37:35 +0200

    feat: implement charging credits logic

--- src/components/auth/AddVisaForm.tsx
+++ src/components/auth/AddVisaForm.tsx
@@ -19,7 +19,6 @@ export default function AddVisaForm() {
   });
 
   async function onSubmit(formData: FieldValues) {
-    console.log(formData);
     try {
       const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/orders/connect-visa-card`, {
         method: 'POST',
@@ -33,9 +32,6 @@ export default function AddVisaForm() {
         credentials: 'include',
       });
       const data = await res.json();
-
-      console.log('res', res);
-      console.log('data', data);
 
       if (!res.ok) {
         throw new Error(`${data.message}`);
@@ -54,7 +50,7 @@ export default function AddVisaForm() {
         description: (err as Error).message,
       });
     }
-    // reset();
+    reset();
   }
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
--- src/components/views/add-crypto-wallet-view.tsx
+++ src/components/views/add-crypto-wallet-view.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { press_start_2p } from '@/constants/fonts';
-import AddVisaForm from '../auth/AddVisaForm';
 import AddCryptoWalletForm from '../auth/AddCryptoWalletForm';
 
 export default function AddCryptoWalletView() {
@@ -9,7 +8,7 @@ export default function AddCryptoWalletView() {
     <Card className="w-[550px] rounded-none border-none bg-stone-950/80 tracking-wider text-stone-100">
       <CardHeader className="text-center">
         <CardTitle className={`${press_start_2p.className} text-md tracking-wider text-red-600`}>
-          Add Crypto Wallet
+          Charge Your Credits
         </CardTitle>
         <CardDescription className="text-stone-100">we guarantee your security</CardDescription>
       </CardHeader>
