33a699487f350e439746440aa6ded206578bb3c4
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-01-21 14:20:44 +0200

    feat: implement get specific product logic

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -4,6 +4,11 @@ import React from 'react';
 const API_URL = process.env.NEXT_PUBLIC_API_URL!;
 export default async function page({ params }: { params: { productId: string } }) {
   const { productId } = await params;
+  console.log(productId);
 
-  return <ProductInfo productId={productId} />;
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products/${productId}`);
+  const data = await res.json();
+  console.log(data);
+
+  return <ProductInfo product={data.product} />;
 }
--- src/components/products/ProductInfo.tsx
+++ src/components/products/ProductInfo.tsx
@@ -5,7 +5,16 @@ import { Button } from '../ui/button';
 import Link from 'next/link';
 import { press_start_2p } from '@/constants/fonts';
 
-export default function ProductInfo({ productId }: { productId: string }) {
+interface ProductType {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  quantity: number;
+  photos: string[];
+}
+
+export default function ProductInfo({ product }: { product: ProductType }) {
   return (
     <div className="mx-auto w-full max-w-xl px-8">
       <h1 className={`${press_start_2p.className} mb-5 text-center text-red-500`}>
@@ -14,26 +23,37 @@ export default function ProductInfo({ productId }: { productId: string }) {
       <ProductImageSlider />
       <div className="flex flex-col gap-5 p-10">
         <p>
-          <span className="font-semibold text-red-600">Product Name: </span> fake money
+          <span className="font-semibold text-red-600">Product Name: </span> {product.name}
         </p>
         <p>
-          <span className="font-semibold text-red-600">Product Description: </span> 1000000 dollars
+          <span className="font-semibold text-red-600">Product Description: </span>{' '}
+          {product.description}
         </p>
         <p>
-          <span className="font-semibold text-red-600">Product sotk: </span> 900
+          <span className="font-semibold text-red-600">Product sotk: </span> {product.quantity}
         </p>
         <p>
-          <span className="font-semibold text-red-600">Product Price: </span> 100$
+          <span className="font-semibold text-red-600">Product Price: </span> {product.price}$
         </p>
 
-        <Link href={`/products/${productId}/order`}>
+        {product.quantity > 0 ? (
+          <Link href={`/products/${product.id}/order`}>
+            <Button
+              variant="ourButton"
+              className="w-full"
+            >
+              order now
+            </Button>
+          </Link>
+        ) : (
           <Button
+            disabled
             variant="ourButton"
             className="w-full"
           >
-            order now
+            Out Of Stock
           </Button>
-        </Link>
+        )}
       </div>
     </div>
   );
