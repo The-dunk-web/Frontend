51283d486d0d7c7bd074e6c284145f6c28c590f1
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-01-21 14:07:45 +0200

    "feat: implement get all products logic"

--- src/components/products/AllProducts.tsx
+++ src/components/products/AllProducts.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 import ProductCard from './ProductCard';
 
+interface ProductType {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  quantity: number;
+  photos: string[];
+}
+
 export default function AllProducts() {
+  const [products, setProducts] = useState<ProductType[] | []>([]);
+
+  useEffect(() => {
+    async function getAllProducts() {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products/`);
+      const data = await res.json();
+      console.log(data.products);
+      setProducts(data.products);
+    }
+
+    getAllProducts();
+  }, []);
   return (
     <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      <ProductCard />
+      {products.length > 0
+        ? products.map((product) => (
+            <ProductCard
+              key={product.id}
+              product={product}
+            />
+          ))
+        : 'Loading'}
     </div>
   );
 }
